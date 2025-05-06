51d5b283fc61b7f32385869adb8611a126cf8fe5
soksreng <csoksreng@gmail.com>
2025-05-06 14:07:23 -0500

    create register and log in function, and token generation logic

---
+++ controllers/authController.js
@@ -0,0 +1,54 @@
+
+const User = require("../models/User");
+const generateToken = require("../utils/generateToken");
+// Register a new user
+exports.register = async (req, res) => {
+    const {name, email, password} = req.body;
+    try{
+        //check if user already exists
+        const userExists = await User.findOne({email});
+        //if user exists, return Email already exists message
+        if(userExists){
+            return res.status(400).json({message: "Email already exists"});
+        }
+        //create new user
+        const user = await User.create({name, email, password});
+        //if user is created successfully, return user details and token
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token: generateToken(user._id),
+        });
+    } catch(error){
+        //if there is an error, return error message
+        res.status(500).json({ error: error.message });
+    }
+};
+
+// Login a user
+exports.login = async (req, res) => {
+    const {email, password} = req.body;
+    try{
+        //find user by email
+        const user = await User.findOne({email});
+        //if user found and password matches, return user details and token
+        if(user && (await user.comparePassword(password))){
+            res.status(200).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                token: generateToken(user._id),
+            });
+        } else {
+            //if user not found or password does not match, return Invalid credentials message
+            res.status(401).json({message: "Invalid credentials"});
+        }
+    }
+    catch(error){
+        //if there is an error, return error message
+        res.status(500).json({ error: error.message });
+    }
+}
---
+++ utils/generateToken.js
@@ -0,0 +1,4 @@
+// This function generates a JWT token using the user's id and a secret key.
+// It uses the jsonwebtoken library to create a token that expires in 7 days.
+const jwt = require('jsonwebtoken');
+module.export = (id) => jwt.sign({id}, process.env.JWT_SECRET, { expiresIn: '7d' })
